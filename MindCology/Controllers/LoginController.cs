7fe002fdaf9f4b4c71ca87b75a726c0b9abb1d53
Lama <lamashabana228@gmail.com>
2021-03-23 21:57:15 +0200

    api-login

--- MindCology/Controllers/LoginController.cs
+++ MindCology/Controllers/LoginController.cs
@@ -6,7 +6,7 @@ using WebApi.Services;
 namespace MindCology.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
         private IUserService _userService;
